f1183dc9dd00f6ec68845f75114baa5dfa9f9b52
Powerkyria <gitteando@gmail.com>
2020-09-28 12:40:17 +0100

    spaceCount Works

--- segundointento/main.js
+++ segundointento/main.js
@@ -4,15 +4,35 @@
 
 function wordsCounter(text){
 
-if(text === ''){
-    return 0;
-}else if( text !== ''){
-    return 1;
+    if(text === ''){
+        return 0;
+    }else if( text !== ''){
+        return 1;
+    }else if(text ){
+        return spaceCount(text)+1;
+        
+    }
 }
 
+function spaceCount(text){
+    let acc=0;
+
+    for(let i=0; i<= text.length; i++){
+        if(text.charAt(i) === ' '){
+        acc = acc+1;
+        }
+    }
+    return acc;
 }
+
+const counter= spaceCount('ordenador de segunda mano viejo y problematico');
+console.log(counter);
+
 
 const case1= wordsCounter('');
 // console.log(case1);
 const case2= wordsCounter('Templo');
-console.log(case2);
+// console.log(case2);
+const case3= wordsCounter('ordenador de segunda mano');
+// console.log(case3);
+
