75a63bcaa107b92ae78cc3cf0ae1891958d93bab
Powerkyria <gitteando@gmail.com>
2020-09-28 12:10:22 +0100

    case1 and case2 done

---
+++ segundointento/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Dado un string decir cuantas palabras hay
+
+function wordsCounter(text){
+
+if(text === ''){
+    return 0;
+}else if( text !== ''){
+    return 1;
+}
+
+}
+
+const case1= wordsCounter('');
+// console.log(case1);
+const case2= wordsCounter('Templo');
+console.log(case2);
