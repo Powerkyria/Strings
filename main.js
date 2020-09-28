fccd6cc8c1249bd7907e38353c5e37b16b922f1f
Powerkyria <gitteando@gmail.com>
2020-09-28 10:15:20 +0100

    second commit

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-"use strict";
+'use strict';
 
+// function countChart(p) {
+// 	return p.length;
+// }
+
+// let number = countChart('visual');
+// console.log(number);
+
+// function word(word, letter) {
+// 	return word.indexOf(letter);
+// }
+// let letterNumber = word('algo', 'y');
+// console.log(letterNumber);
+
+function one(text) {
+	if (text === '') {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+const wordNumber = one('Mi pc');
+console.log(wordNumber);
