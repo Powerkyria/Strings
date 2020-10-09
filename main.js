f77ddb71510141aba98c556e8ab790fa05d96f1f
Powerkyria <gitteando@gmail.com>
2020-10-09 09:57:54 +0100

    ahorcado functions

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// FUNCIONES QUE SIRVEN PARA EL AHORCADO
+
+// 1. Contar letras
+
 // function countChart(p) {
 // 	return p.length;
 // }
@@ -7,34 +11,38 @@
 // let number = countChart('visual');
 // console.log(number);
 
+// 2. En que posicion se encuentra una letra dentro de la palabra
+
 // function word(word, letter) {
 // 	return word.indexOf(letter);
 // }
 // let letterNumber = word('algo', 'y');
 // console.log(letterNumber);
 
-function wordCount(text) {
-	if (text === '') {
-		return 0;
-	} else if (text.includes(' ')) {
-		return countSpace(text) +1;
-	} else if (text != '' && !text.includes(' ')) {
-		return 1;
-	}
-}
+// 3. Contar cuantas palabras hay
 
-const wordNumber = wordCount('Mi casa nueva amarilla');
-console.log(wordNumber);
+// function wordCount(text) {
+// 	if (text === '') {
+// 		return 0;
+// 	} else if (text.includes(' ')) {
+// 		return countSpace(text) +1;
+// 	} else if (text != '' && !text.includes(' ')) {
+// 		return 1;
+// 	}
+// }
 
-function countSpace(text) {
-    let acc= 0;
-	for(let i = 0; i <= text.length; i++) {
-        if(text.charAt(i) === ' '){
-            acc = acc + 1;
-        }
-    }
-    return acc;
-}
+// const wordNumber = wordCount('Mi casa nueva amarilla');
+// console.log(wordNumber);
 
-const spaceNumber = countSpace('Mi casa nueva');
+// function countSpace(text) {
+//     let acc= 0;
+// 	for(let i = 0; i <= text.length; i++) {
+//         if(text.charAt(i) === ' '){
+//             acc = acc + 1;
+//         }
+//     }
+//     return acc;
+// }
+
+// const spaceNumber = countSpace('Mi casa nueva');
 // console.log(spaceNumber);
