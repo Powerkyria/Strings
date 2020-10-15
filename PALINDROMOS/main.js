6eb9bb394d89220740e5121111a5918f360d3e52
Powerkyria <gitteando@gmail.com>
2020-10-15 11:22:07 +0100

    add function without array

--- PALINDROMOS/main.js
+++ PALINDROMOS/main.js
@@ -6,8 +6,6 @@ function palindromo(word) {
 	const upperWord = word.toUpperCase();
 	const reverseWord = upperWord.split('').reverse().join('');
 
-console.log(reverseWord);
-
 	if (reverseWord === upperWord) {
 		return 'Es un Palindromo';
 	} else {
@@ -20,26 +18,46 @@ console.log(myPalindromo);
 
 // 2.2 CREA UNA FUNCION QUE ACEPTE UNA STRING POR PARAMETRO Y QUE DIGA SI ESA STRING ES UN PALINDROMO SIN SPLIT NI REVERSE
 
-function newPalindromo(word) {
+// function newPalindromo(word) {
+// 	const upperWord = word.toUpperCase();
+// 	const arrayOfLetters = Array.from(upperWord);
+// 	const arrayReverse = [];
+
+// 	for (let i = 0; i < upperWord.length; i++) {
+// 		const reverseWord = arrayOfLetters.pop();
+// 		arrayReverse.push(reverseWord);
+// 	}
+// 	const joinArrayReverse = arrayReverse.join('');
+// 	console.log(arrayReverse);
+// 	console.log(joinArrayReverse);
+
+// 	if (joinArrayReverse === upperWord) {
+// 		return 'Es un Palindromo';
+// 	} else {
+// 		return 'No es un palindromo';
+// 	}
+// }
+
+// const mySecondPalindromo = newPalindromo('Ana');
+// console.log(mySecondPalindromo);
+
+// SIN CONVERTIR EL STRING EN ARRAY JG
+
+function isPalindromo2(word) {
 	const upperWord = word.toUpperCase();
-	const arrayOfLetters = Array.from(upperWord);
-	const arrayReverse = [];
+	let stringReverse = '';
 
-	for (let i = 0; i < upperWord.length; i++) {
-		const reverseWord = arrayOfLetters.pop();
-		arrayReverse.push(reverseWord);
+	for (let i = upperWord.length; i > -1; i--) {
+		const reverseWord = upperWord.charAt(i);
+		stringReverse = stringReverse + reverseWord;
 	}
-	const joinArrayReverse = arrayReverse.join('');
-	console.log(arrayReverse);
-	console.log(joinArrayReverse);
 
-	if (joinArrayReverse === upperWord) {
+	if (stringReverse === upperWord) {
 		return 'Es un Palindromo';
 	} else {
 		return 'No es un palindromo';
 	}
 }
 
-const mySecondPalindromo = newPalindromo('Ana');
-console.log(mySecondPalindromo);
-
+const mySecondPalindromo2 = isPalindromo2('Ana');
+console.log(mySecondPalindromo2);
