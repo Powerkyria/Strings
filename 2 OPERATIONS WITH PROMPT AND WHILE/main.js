9513e64b0ac2a0fa44e20cb3c9e1729fe30f6acd
Powerkyria <gitteando@gmail.com>
2020-10-27 22:01:29 +0000

    ask for separate numbers

--- 2 OPERATIONS WITH PROMPT AND WHILE/main.js
+++ 2 OPERATIONS WITH PROMPT AND WHILE/main.js
@@ -2,20 +2,28 @@
 
 // 2.Haz un prompt donde aparezca las siguientes opciones. 1.Sumar,2.Restar,3.Multiplicar,4.Dividir,5.Salir . Una vez elegida saldrá otro prompt donde el usuario ingresará dos numero separandolos por "-" ejemplo: "2-8" o "10-45". Un vez hecho ejecutar la accion apropiada tras lo cual deberas mostrar el resultado en un alert con el siguiente formato "El resultado de multiplicar 2 por 8 es 16" o "el resultado de restar 2 mas 8 es 10" asi por cada operación. Tras cerrar el modal. Se volverá al menú inicial para que el usuario escoja una opcion. Y en caso de selecionar la opcion 5 salir el programa se cerrará.
 
-//INTENTO FALLIDO DE PEDIR DOS NUMEROS SEPARADOS X UN GUION
-// const hisNumbers = 'Ingrese dos números separados por un guión'
-//  let x,y = prompt(hisNumbers, ''+"-"+'')
-
 const message = `Eliga una opción:
 
 1.Sumar, 2.Restar, 3.Multiplicar, 4.Dividir, 5.Salir`;
 
 let response = prompt(message);
 
-const messageNumberOne = 'Ingrese un número';
-const messageNumberTwo = 'Ingrese otro número';
-let x = prompt(messageNumberOne);
-let y = prompt(messageNumberTwo);
+//PEDIR UN NUMERO CADA VEZ
+// const messageNumberOne = 'Ingrese un número';
+// const messageNumberTwo = 'Ingrese otro número';
+// let x = prompt(messageNumberOne);
+// let y = prompt(messageNumberTwo);
+
+
+// //PEDIR DOS NUMEROS SEPARADOS X UN GUION
+const askForNumbers = 'Ingrese dos números separados por un guión';
+let numbers = prompt(askForNumbers, ''+'-'+'');
+
+const indexOfX= numbers.indexOf('-') - 1;
+const indexOfY= numbers.indexOf('-') + 1;
+let x = numbers.charAt(indexOfX);
+let y = numbers.charAt(indexOfY);
+
 const exitMessage = 'salir';
 
 while (response.toLowerCase() != exitMessage || response != 5) {
