6d9618608821f6ad3db0b4b89360abf7d0aeb4c9
Powerkyria <gitteando@gmail.com>
2020-10-29 12:46:21 +0000

    add solution with bubble method

--- ORDER ARRAY OF NUMBERS/main.js
+++ ORDER ARRAY OF NUMBERS/main.js
@@ -11,9 +11,7 @@ function orderNumbers(numbers) {
 
 	for (let i = 0; (i = numbers.length); i++) {
 		let maxNumber = Math.max(...numbers);
-
 		const index = numbers.indexOf(maxNumber);
-
 		const deleteMaxNumber = +numbers.splice(index, 1);
 		numbersInOrder.push(deleteMaxNumber);
 	}
@@ -28,19 +26,35 @@ const result = orderNumbers(randomNumbers);
 const myNumbers = [ 23, 34, 5, 10, 8 ];
 
 function orderNumbersWithFor(numbers) {
-	let numbersInOrder = [];
-
 	for (let i = 0; i < numbers.length; i++) {
-		for (let j = 1; j < numbers.length; j++) {
-			if (numbers[i] <= numbers[j]) {	
-				
-				numbersInOrder.push(numbers[i] );
+		for (let j = 0; j < numbers.length - 1; j++) {
+			if (numbers[j] < numbers[j + 1]) {
+				let aux = numbers[j];
+				numbers[j] = numbers[j + 1];
+				numbers[j + 1] = aux;
 			}
-			console.log(numbers[i] + '-' + numbers[j]);
 		}
 	}
-	return numbersInOrder;
+	return myNumbers;
 }
 
 const result2 = orderNumbersWithFor(myNumbers);
 console.log(result2);
+
+// SOLUCION JG
+
+function order() {
+	let numbers = [ 389, 703, 247, 563, 224, 714, 464, 953, 708, 201, 887, 550, 515, 206, 131 ];
+
+	for (let j = 0; j < numbers.length; j++) {
+		for (let i = 0; i < numbers.length - 1; i++) {
+			if (numbers[i] > numbers[i + 1]) {
+				let aux = numbers[i];
+				numbers[i] = numbers[i + 1];
+				numbers[i + 1] = aux;
+			}
+		}
+	}
+	// console.log(numbers);
+}
+order();
