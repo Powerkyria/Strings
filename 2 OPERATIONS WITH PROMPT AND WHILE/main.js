77600e9a5c98204f9f4f7427fe111a409a7cd97d
Powerkyria <gitteando@gmail.com>
2020-10-29 11:45:29 +0000

    patch introduce variables into while

--- 2 OPERATIONS WITH PROMPT AND WHILE/main.js
+++ 2 OPERATIONS WITH PROMPT AND WHILE/main.js
@@ -2,6 +2,15 @@
 
 // 2.Haz un prompt donde aparezca las siguientes opciones. 1.Sumar,2.Restar,3.Multiplicar,4.Dividir,5.Salir . Una vez elegida saldrá otro prompt donde el usuario ingresará dos numero separandolos por "-" ejemplo: "2-8" o "10-45". Un vez hecho ejecutar la accion apropiada tras lo cual deberas mostrar el resultado en un alert con el siguiente formato "El resultado de multiplicar 2 por 8 es 16" o "el resultado de restar 2 mas 8 es 10" asi por cada operación. Tras cerrar el modal. Se volverá al menú inicial para que el usuario escoja una opcion. Y en caso de selecionar la opcion 5 salir el programa se cerrará.
 
+// //PEDIR DOS NUMEROS SEPARADOS X UN GUION
+// const askForNumbers = 'Ingrese dos números separados por un guión';
+// let numbers = prompt(askForNumbers, ''+'-'+'');
+
+// const indexOfX= numbers.indexOf('-') - 1;
+// const indexOfY= numbers.indexOf('-') + 1;
+// let x = numbers.charAt(indexOfX);
+// let y = numbers.charAt(indexOfY);
+
 const message = `Eliga una opción:
 
 1.Sumar, 2.Restar, 3.Multiplicar, 4.Dividir, 5.Salir`;
@@ -9,37 +18,35 @@ const message = `Eliga una opción:
 let response = prompt(message);
 
 //PEDIR UN NUMERO CADA VEZ
-// const messageNumberOne = 'Ingrese un número';
-// const messageNumberTwo = 'Ingrese otro número';
-// let x = prompt(messageNumberOne);
-// let y = prompt(messageNumberTwo);
-
-
-// //PEDIR DOS NUMEROS SEPARADOS X UN GUION
-const askForNumbers = 'Ingrese dos números separados por un guión';
-let numbers = prompt(askForNumbers, ''+'-'+'');
-
-const indexOfX= numbers.indexOf('-') - 1;
-const indexOfY= numbers.indexOf('-') + 1;
-let x = numbers.charAt(indexOfX);
-let y = numbers.charAt(indexOfY);
+const messageNumberOne = 'Ingrese un número';
+const messageNumberTwo = 'Ingrese otro número';
+let x;
+let y;
 
 const exitMessage = 'salir';
 
 while (response.toLowerCase() != exitMessage || response != 5) {
 	if (response.toLowerCase() === 'sumar' || response == 1) {
+		x = prompt(messageNumberOne);
+		y = prompt(messageNumberTwo);
 		add();
 	} else if (response.toLowerCase() === 'restar' || response == 2) {
+		x = prompt(messageNumberOne);
+		y = prompt(messageNumberTwo);
 		subtract();
 	} else if (response.toLowerCase() === 'multiplicar' || response == 3) {
+		x = prompt(messageNumberOne);
+		y = prompt(messageNumberTwo);
 		multiply();
 	} else if (response.toLowerCase() === 'dividir' || response == 4) {
+		x = prompt(messageNumberOne);
+		y = prompt(messageNumberTwo);
 		divide();
 	}
 }
 
 function add() {
-	let resultA = parseInt(x) + parseInt(y);
+	let resultA = Number(x) + Number(y);
 	alert(`El resultado de sumar ${x} mas ${y} es ${resultA}`);
 	return resultA;
 }
