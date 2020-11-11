3b14572935a90ad034c2fde2f8c07ba761dd58db
Powerkyria <gitteando@gmail.com>
2020-11-11 08:44:01 +0000

    add new task

--- FIBONACHI/main.js
+++ FIBONACHI/main.js
@@ -1,2 +1,11 @@
-
 'use strict';
+
+// 1.Crea dos funciones que imprima los primeros 100 de la secuencia Fibonacci. Una función debe funcionar de manera iterativa y otra de manera recursiva.
+
+function printNumbers(){
+
+}
+
+printNumbers()
+
+// 2. Ahora haz que estas dos funciones sean parametrizables. Y en lugar de imprimir los 100 primeros números, imprimirá la cantidad de números que se le pasen por parámetro.
