60b25d4340882f65572b6973990788fcb8e14ba1
Powerkyria <gitteando@gmail.com>
2020-12-03 22:01:06 +0000

    done architecture two

--- ARCHITECTURE2/main.js
+++ ARCHITECTURE2/main.js
@@ -1,31 +1,63 @@
 'use strict';
 
+/* 1. Crea 3 clases View, Client y Presenter.
 
-// 1.Crea tres clases. View, Presenter y Client.
-// Tambien vamos a usar la api de star wars para traernos algunos personajes de la pelicula:
-// https://swapi.dev/api/people
+- Client tendrá un método findStarWarsPeople q hará la peticion a la API: https://swapi.dev/api/people y pasará un callback por parametro.
 
-// La clase presenter va aceptar por el constructor las clases view y client, tendrá un metodo Execute.
-// Este metodo tomará la clase client, usará el metodo de la instancia de client,FindStarWarsPeople.
+- View tendrá un método showPeople q cogerá la info y la pintará en el html: name, birth_year, eye_color, height.
 
-// El metodo de la clase cliente FindStarWarsPeople tomará un funcion como parametro, un callback.
-// Una vez se traiga los datos pasará al callback dicha información.
+- Presenter tendrá por parametro las clases view y client y un método execute q tomará la clase client y usará su método,findStarWarsPeople q pasará por parametro un callback q recibirá solo la info de los personajes de la API q mostrará y se los pasará al método de view, showPeople.
 
-// El presenter le pasará por parametro al metodo FindStarWarsPeople un callback.
-// Dicho callback recibirá la información de los personajes de star wars
-// y se los pasará a un metodo de la vista(view), showPeople.
-// El metodo showPeople cogerá la informacion y la pintará en el html.
-// Mostrando, name, birth_year, eye_color, height
+Consejos: Empieza por la clase client, luego programa la vista y por último conéctalos a través del presenter */
 
-// Basicamente esta arquitectura ayuda a separar la persistencia  "el cliente" de "la vista" y conecta ambos por medio de un presenter
-// el html que uses para el ejercicio, puede ser sin estilos. Y solo con los texto en una caja
+(() => {
+	function Client() {
+		return {findStarWarsPeople};
+		function findStarWarsPeople(callback) {
+            fetch('https://swapi.dev/api/people')
+            .then(response => response.json())
+            .then(data => {
+				callback(data.results);
+			});
+		}
+	}
 
-// practicamente esta arquitectura la puedes usar para cualquier front que hagas y no quieras utilizar frameworks.
+	function View() {
+		return {showPeople};
+		function showPeople(people) {
+			for (const person of people) {
+				let HTMLcode = `<ul>
+                <li>${person.name}</li>
+                <li>${person.birth_year}</li>
+                <li>${person.eye_color}</li>
+                <li>${person.height}</li></ul>`;
+				document.getElementById('people').innerHTML += HTMLcode;
+			}
+		}
+	}
 
-// Para componente con comportamiento en el html, lo puedes gestionar con una vista y un presenter. Cuando necesites tratar con informacion que persista, ya sea una api o un archivo, puedes usar un cliente o modelo para esta funcion.
-
-// Consejos: empieza por la clase client, y has pruebas para ver si funciona la llamada a la api.Tras lo cual programa la vista pasandole datos falsos. Y por ultimo los conectes a traves del presenter
-
-(function() {
-
+	function Presenter(client, view) {
+		return {execute};
+		function execute() {
+			client.findStarWarsPeople(showPersonFeatures);
+		}
+		function showPersonFeatures(results) {
+			const people = results.map(person => {
+				return {
+					name: person.name,
+					birth_year: person.birth_year,
+					eye_color: person.eye_color,
+					height: person.height,
+				};
+            });
+			view.showPeople(people);
+		}
+	}
+	function main() {
+		let client = Client();
+		let view = View();
+		let presenter = Presenter(client, view);
+		presenter.execute();
+	}
+	main();
 })();
